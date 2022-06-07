a09e804c627cd047d623aae902efb248cbf15c45
市拓人 <shitakuto@shitakutonoMacBook-Air.local>
2022-06-08 05:40:59 +0900

    java2

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,16 @@
+
+public class Chapter04 {
+	
+	public static void main(String[] args) {
+		int number;
+		String name;
+		number = 7;
+		name = "市拓人";
+		System.out.println(number);
+		System.out.println(name);
+		
+		name = "天は二物を与えない";
+		System.out.println(name);
+	}
+
+}
