fa7583abe879759174ec0e93e108ca7d7ea9c686
市拓人 <shitakuto@shitakutonoMacBook-Air.local>
2022-06-08 07:23:16 +0900

    java3

---
+++ JavaWcp/src/Chapter05.java
@@ -0,0 +1,26 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long long1 = 777777777L;
+		System.out.println(long1);
+		
+		double double1 = 777777.77d;
+		System.out.println(double1);
+		
+		boolean boolean1 = false;
+		System.out.println(boolean1);
+		
+		Float f = 0.123f;
+		System.out.println(f);
+		
+		Integer i = 100;
+		System.out.println(i);
+		
+		Character c = 'a';
+		System.out.println(c);
+		
+		int[] array1 = new int[] { 10, 20, 30, 40 };
+		System.out.println(array1[3]);
+	}
+
+}
